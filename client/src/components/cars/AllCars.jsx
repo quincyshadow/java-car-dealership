c3040d5c0256e8073d274b784ef8dd860f3617c1
quincyshadow <inversenormal@gmail.com>
2019-09-05 12:07:55 -0700

    cars are displayedon cards

--- client/src/components/cars/AllCars.jsx
+++ client/src/components/cars/AllCars.jsx
@@ -1,27 +1,26 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Row, Col } from 'reactstrap'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Card, Button, CardImg, CardTitle, CardText, CardColumns,
+ CardSubtitle, CardBody } from "reactstrap";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-import { getAllCars } from '../../store/cars/actions'
+import { getAllCars } from "../../store/cars/actions";
 
 const AllCars = props => {
-
   const styles = {
     rowStyles: {
-      marginTop: '22vh'
+      marginTop: "22vh"
     }
-  }
+  };
 
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   let getCars = async function() {
     // call our action creator
-    dispatch(getAllCars( null, props.history));
-  }
+    dispatch(getAllCars(null, props.history));
+  };
 
   // const [cars, setCars] = useState({});
-  // 
+  //
   // useEffect(() => {
   //   getCars();
   // }, []);
@@ -29,15 +28,47 @@ const AllCars = props => {
   getCars();
 
   const carList = useSelector(state => state.cars.carList);
+  let carItems = carList.map((item) => {
+    let card = (
+      <Card key={item.id} className="col-4">
+        <CardImg
+          top
+          width="100%"
+          src={item.photoUrl}
+          alt={`${item.make} ${item.model}`}
+        />
+        <CardBody>
+          <CardTitle>
+            {item.year} {item.make} {item.model}
+          </CardTitle>
+          <CardSubtitle>${item.price}</CardSubtitle>
+          <CardText>
+            <ul>
+              <li>VIN: {item.vin}</li>
+              <li>Miles: {item.miles}</li>
+              <li>Location: {item.locationId}</li>
+            </ul>
+          </CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    );
+
+    return card;
+
+    //{"id":1,"vin":"ASD123","year":2019,"make":"Chevroley","model":"Cruze","miles":23,"price":20500,"photoUrl":null,"locationId":1}
+  });
 
   return (
     <Container>
       {/* {console.log(cars)} */}
-          {JSON.stringify(carList[0])}
-          {console.log("CARLIST ----")}
-          {console.log(carList)}
+      <Row>{carItems}</Row>
 
+      {/* {JSON.stringify(carList[0])} */}
+      {/* {console.log("CARLIST ----")} */}
+      {/* {console.log(carItems)} */}
+      
     </Container>
-  )
-}
-export default AllCars
+  );
+};
+export default AllCars;
